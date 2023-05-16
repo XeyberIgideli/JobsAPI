ee749844b78156824e082fb2b3c928e5d1f7918a
XeyberIgideli <d3facerx@gmail.com>
2023-05-16 18:33:46 +0400

    some security measures

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import { verifyToken } from './middlewares/Auth.js'
+import { errorHandlerMiddleware } from './middlewares/Error.js'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+
+// Security packages
+import helmet from 'helmet'
+import cors from 'cors'
+import xss from 'xss-clean'
+import rateLimiter from 'express-rate-limit'
 // Routes import
 import jobRoute from './routes/jobRoute.js'
 import authRoute from './routes/authRoute.js'
-import { errorHandlerMiddleware } from './middlewares/Error.js'
 
 const port = process.env.PORT || 9000
 const app = express()
@@ -21,6 +27,16 @@ mongoose.connect(process.env.MONGO_URI, {
 // Middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
+app.use(helmet())
+app.use(xss())
+app.use(cors())
 
 // Routes
 app.get('/',(req,res) => {res.send('Jobs API portal')})
